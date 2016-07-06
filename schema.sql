46ae8f65d72037106113580db1d4de018c64e252
Matthew Allen Go <matthew_allen_go@dlsu.edu.ph>
2016-07-06 10:42:42 +0800

    Implement server with database

---
+++ schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE note (
+  id         int(11)   PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  note       text      NOT NULL,
+  timestamp  datetime  NOT NULL
+);
--- web/index.php
+++ web/index.php
@@ -2,31 +2,88 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = new Silex\Application();
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+$app = new Application();
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'dbname' => 'web_api_prototype_db',
+        'user' => 'root',
+        // 'password' => 'password',
+        'host' => 'localhost',
+        'driver' => 'pdo_mysql',
+    ),
+));
+
 
 $app->get('/', function() use($app) {
-    $status = array('status' => 'ok');
-    return $app->json($status);
+    return $app->json('ok');
 });
 
+// list
 $app->get('/notes', function() use($app) {
-    return 'list of notes';
+    $conn = $app['db'];
+    $query = "SELECT id, note FROM note ORDER BY timestamp desc";
+
+    $notes = $conn->fetchAll($query);
+    foreach ($notes as &$note) {
+        $text = $note['note'];
+        if (strlen($text) > 40) {
+            $note['note'] = substr($text, 0, 40-3).'...';
+        }
+    }
+
+    return $app->json($notes);
 });
 
-$app->post('/note', function() use($app) {
-    return 'create note';
+// create
+$app->post('/note', function(Request $req) use($app) {
+    $conn = $app['db'];
+
+    $note = array(
+        'note' => $req->get('note'),
+        'timestamp' => date('Y-m-d H:i:s'),
+    );
+
+    $conn->insert('note', $note);
+    $id = $conn->lastInsertId();
+
+    return $app->json($id);
 });
 
+// retrieve
 $app->get('/note/{id}', function($id) use($app) {
-    return 'retrieve note';
+    $conn = $app['db'];
+    $query = "SELECT * FROM note WHERE id = ?";
+
+    $note = $conn->fetchAssoc($query, array($id));
+
+    return $app->json($note);
 });
 
-$app->put('/note/{id}', function($id) use($app) {
-    return 'update note';
+// update
+$app->put('/note/{id}', function($id, Request $req) use($app) {
+    $conn = $app['db'];
+
+    $note = array(
+        'note' => $req->get('note'),
+        'timestamp' => date('Y-m-d H:i:s'),
+    );
+
+    $conn->update('note', $note, array('id' => $id));
+
+    return $app->json('ok');
 });
 
+// delete
 $app->delete('/note/{id}', function($id) use($app) {
-    return 'delete note';
+    $conn = $app['db'];
+
+    $conn->delete('note', array('id' => $id));
+
+    return $app->json('ok');
 });
 
 
