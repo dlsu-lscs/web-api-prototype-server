a92289e75c17de11c50f53648e62eac5c27001db
Matthew Allen Go <matthew_allen_go@dlsu.edu.ph>
2016-07-06 20:38:31 +0800

    Switch to class methods instead of anonymous functions

--- web/index.php
+++ web/index.php
@@ -17,87 +17,89 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     ),
 ));
 
-
 $app->get('/', function() use($app) {
     return $app->json('ok');
 });
 
-// list
-$app->get('/notes', function() use($app) {
-    $conn = $app['db'];
-    $query = "SELECT id, note FROM note ORDER BY timestamp desc";
-
-    $notes = $conn->fetchAll($query);
-    foreach ($notes as &$note) {
-        $text = $note['note'];
-        if (strlen($text) > 40) {
-            $note['note'] = substr($text, 0, 40-3).'...';
-        }
-    }
-
-    return $app->json($notes);
-});
-
-// create
-$app->post('/note', function(Request $req) use($app) {
-    $conn = $app['db'];
-
-    $note = array(
-        'note' => $req->get('note'),
-        'timestamp' => date('Y-m-d H:i:s'),
-    );
-
-    $conn->insert('note', $note);
-    $id = $conn->lastInsertId();
-
-    return $app->json($id);
-});
-
-// retrieve
-$app->get('/note/{id}', function($id) use($app) {
-    $conn = $app['db'];
-    $query = "SELECT * FROM note WHERE id = ?";
-
-    $note = $conn->fetchAssoc($query, array($id));
-    if (!$note) {
-        $error = array('message' => 'note not found');
-        return $app->json($error, 404);
-    }
-
-    return $app->json($note);
-});
-
-// update
-$app->put('/note/{id}', function($id, Request $req) use($app) {
-    $conn = $app['db'];
-
-    $note = array(
-        'note' => $req->get('note'),
-        'timestamp' => date('Y-m-d H:i:s'),
-    );
-
-    $ok = $conn->update('note', $note, array('id' => $id));
-    if (!$ok) {
-        $error = array('message' => 'note not found');
-        return $app->json($error, 404);
-    }
-
-    return $app->json('ok');
-});
-
-// delete
-$app->delete('/note/{id}', function($id) use($app) {
-    $conn = $app['db'];
-
-    $ok = $conn->delete('note', array('id' => $id));
-    if (!$ok) {
-        $error = array('message' => 'note not found');
-        return $app->json($error, 404);
-    }
-
-    return $app->json('ok');
-});
-
+$app->get('/notes', 'Notes::index');
+$app->post('/note', 'Notes::create');
+$app->get('/note/{id}', 'Notes::retrieve');
+$app->put('/note/{id}', 'Notes::update');
+$app->delete('/note/{id}', 'Notes::delete');
 
 $app['debug'] = true;
 $app->run();
+
+
+class Notes {
+    public function index(Application $app, Request $req) {
+        $conn = $app['db'];
+        $query = "SELECT id, note FROM note ORDER BY timestamp desc";
+
+        $notes = $conn->fetchAll($query);
+        foreach ($notes as &$note) {
+            $text = $note['note'];
+            if (strlen($text) > 40) {
+                $note['note'] = substr($text, 0, 40-3).'...';
+            }
+        }
+
+        return $app->json($notes);
+    }
+
+    public function create(Application $app, Request $req) {
+        $conn = $app['db'];
+
+        $note = array(
+            'note' => $req->get('note'),
+            'timestamp' => date('Y-m-d H:i:s'),
+        );
+
+        $conn->insert('note', $note);
+        $id = $conn->lastInsertId();
+
+        return $app->json($id);
+    }
+
+    public function retrieve(Application $app, Request $req, $id) {
+        $conn = $app['db'];
+        $query = "SELECT * FROM note WHERE id = ?";
+
+        $note = $conn->fetchAssoc($query, array($id));
+        if (!$note) {
+            $error = array('message' => 'note not found');
+            return $app->json($error, 404);
+        }
+
+        return $app->json($note);
+    }
+
+    public function update(Application $app, Request $req, $id) {
+        $conn = $app['db'];
+
+        $note = array(
+            'note' => $req->get('note'),
+            'timestamp' => date('Y-m-d H:i:s'),
+        );
+
+        $ok = $conn->update('note', $note, array('id' => $id));
+        if (!$ok) {
+            $error = array('message' => 'note not found');
+            return $app->json($error, 404);
+        }
+
+        return $app->json('ok');
+    }
+
+    public function delete(Application $app, Request $req, $id) {
+        $conn = $app['db'];
+
+        $ok = $conn->delete('note', array('id' => $id));
+        if (!$ok) {
+            $error = array('message' => 'note not found');
+            return $app->json($error, 404);
+        }
+
+        return $app->json('ok');
+    }
+}
