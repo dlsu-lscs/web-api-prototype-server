0acbb7a6085010a8b23b13684b97b41eea09f932
Matthew Allen Go <matthew_allen_go@dlsu.edu.ph>
2016-07-06 09:04:16 +0800

    Create server skeleton

--- web/index.php
+++ web/index.php
@@ -4,9 +4,31 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
 
-$app->get('/hello/{name}', function($name) use($app) {
-    return 'Hello '.$app->escape($name);
+$app->get('/', function() use($app) {
+    $status = array('status' => 'ok');
+    return $app->json($status);
 });
+
+$app->get('/notes', function() use($app) {
+    return 'list of notes';
+});
+
+$app->post('/note', function() use($app) {
+    return 'create note';
+});
+
+$app->get('/note/{id}', function($id) use($app) {
+    return 'retrieve note';
+});
+
+$app->put('/note/{id}', function($id) use($app) {
+    return 'update note';
+});
+
+$app->delete('/note/{id}', function($id) use($app) {
+    return 'delete note';
+});
+
 
 $app['debug'] = true;
 $app->run();
